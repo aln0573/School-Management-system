c8fe507be38fc83e44ecf895bc7b9c92a9c7d4fc
afreedKh <www.afreedappi599@gmail.com>
2026-01-03 23:35:35 +0530

    feat: Add authorization middleware to manage user role access

---
+++ server/src/presentation/middleware/authorize.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { UserRole } from "../../domain/entities/User";
+
+export const authorize = (...allowedRoles: UserRole[]) => {
+   (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    if (!allowedRoles.includes(user.role)) {
+      return res.status(403).json({ message: "Forbidden" });
+    }
+
+    next();
+  };
+};
